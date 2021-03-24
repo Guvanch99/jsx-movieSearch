1c13e54e61d88123870c8848f5276c71f5872156
Guvanch Avediyev <awediyewguwanc@gmail.com>
2021-03-24 14:46:10 -0700

    Updates

---
+++ precache-manifest.0b98d261ebfef9e8422d8655c10115ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72bece0eea0e9e5f3dbc320e4265ec82",
+    "url": "/jsx-movieSearch/index.html"
+  },
+  {
+    "revision": "fe28bd88470abb63e883",
+    "url": "/jsx-movieSearch/static/css/main.071542c2.chunk.css"
+  },
+  {
+    "revision": "165fca3f5c3d5f9fa6da",
+    "url": "/jsx-movieSearch/static/js/2.fab6d004.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jsx-movieSearch/static/js/2.fab6d004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe28bd88470abb63e883",
+    "url": "/jsx-movieSearch/static/js/main.f91a5b78.chunk.js"
+  },
+  {
+    "revision": "546a13eb3592fe621cc1",
+    "url": "/jsx-movieSearch/static/js/runtime-main.8fdb23b2.js"
+  }
+]);
